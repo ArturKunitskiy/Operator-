4d0218015aea61ab0b243af40672ccae727a2ba3
artur artur <arturk102007@gmail.com>
2024-10-25 16:14:11 +0300

    Add project files.

---
+++ dz.25.10.24/dz.25.10.24.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+private:
+
+	char* name;
+	int age;
+
+public:
+
+	Student();
+	Student(const char* n, int a);
+	~Student();
+	void Print();
+	Student(const Student& obj);
+	void Input();
+	Student& operator=(Student& b);
+};
+
+Student::Student()
+{
+	name = nullptr;
+	age = 0;
+}
+Student::Student(const char* n, int a)
+{
+	name = new char[strlen(n) + 1];
+	strcpy_s(name, strlen(n) + 1, n);
+	age = a;
+}
+Student::~Student()
+{
+	cout << "Destructor\n";
+	delete[] name;
+	age = 0;
+}
+void Student::Print()
+{
+	cout << "Name: " << name << endl << "Age: " << age << endl;
+}
+void Student::Input()
+{
+	char buff[1000];
+	cout << "Name: " << endl;
+	cin >> buff;
+	if (name != nullptr)
+	{
+		delete[] name;
+	}
+
+	name = new char[strlen(buff) + 1];
+	strcpy_s(name, strlen(buff) + 1, buff);
+
+	cout << "Age: " << endl;
+	cin >> age;
+}
+Student::Student(const Student& obj)
+{
+	name = new char[strlen(obj.name) + 1];
+	strcpy_s(name, strlen(obj.name) + 1, obj.name);
+	age = obj.age;
+	cout << "Copy constructor\n" << endl;
+}
+
+Student& Student::operator=(Student& obj)
+{
+	cout << "!!!" << endl;
+	if (this == &obj)
+	{
+		return *this;
+	}
+
+	if (name != nullptr)
+	{
+		delete[] name;
+	}
+
+	name = new char[strlen(obj.name) + 1];
+	strcpy_s(name, strlen(obj.name) + 1, obj.name);
+	age = obj.age;
+
+	return *this;
+}
+
+int main()
+{
+	Student a("Ivan ", 16);
+	a.Print();
+	Student b = a;
+
+	b.Input();
+	b.Print();
+
+	Student c;
+	c = a;
+	c.Print();
+}
